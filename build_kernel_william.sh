93ab9d815382b414f52142008220fb5420fe52ef
williamkosasih19 <williamkosasih19@gmail.com>
2019-12-25 22:07:33 +1030

    added build script

---
+++ build_kernel_william.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+export ARCH=arm64
+export CROSS_COMPILE=/home/williamkosasih/aarch64-linux-android-4.9/bin/aarch64-linux-android-
+exoirt ANDROID_MAJOR_VERSION=p
+export PLATFORM_VERSION=9.0.0
+
+make -C $(pwd) O=$(pwd)/out KCFLAGS=-mno-android gts4llte_eur_open_defconfig
+make -j64 -C $(pwd) O=$(pwd)/out KCFLAGS=-mno-android
+
+cd anyKernel3
+
+rm kernel.zip
+
+cp ../out/arch/arm64/boot/Image.g Image.gz
+cp ../out/arch/arm64/boot/Image.gz-dtb zImage
+
+zip -r kernel.zip .
---
+++ pack_anyKernel.sh
@@ -0,0 +1,7 @@
+cd anyKernel3
+
+rm kernel.zip
+
+cp ../out/arch/arm64/boot/Image.gz-dtb zImage
+
+zip -r kernel.zip .
