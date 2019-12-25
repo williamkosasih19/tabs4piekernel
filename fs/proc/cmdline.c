ca635de7e07e632fbcdc1ace2adf906a71970133
williamkosasih19 <williamkosasih19@gmail.com>
2019-12-25 22:55:16 +1030

    remove safetynet commandline parameter

--- fs/proc/cmdline.c
+++ fs/proc/cmdline.c
@@ -2,10 +2,30 @@
 #include <linux/init.h>
 #include <linux/proc_fs.h>
 #include <linux/seq_file.h>
+#include <asm/setup.h>
+
+char patched_command_line[COMMAND_LINE_SIZE];
 
 static int cmdline_proc_show(struct seq_file *m, void *v)
 {
-	seq_printf(m, "%s\n", saved_command_line);
+	char *needle_address_begin, *needle_address_end;
+	size_t length;
+
+	strcpy(patched_command_line, saved_command_line);
+
+	needle_address_begin = strstr(patched_command_line, "androidboot.verifiedbootstate=");
+	needle_address_end = needle_address_begin;
+
+	length = strlen(saved_command_line - (needle_address_end - saved_command_line) + 1);
+
+	if (needle_address_end)
+	{
+		while (*needle_address_end++ != ' ')
+			;
+		memmove(needle_address_begin, needle_address_end, length);
+	}
+
+	seq_printf(m, "%s\n", patched_command_line);
 	return 0;
 }
 
@@ -15,10 +35,10 @@ static int cmdline_proc_open(struct inode *inode, struct file *file)
 }
 
 static const struct file_operations cmdline_proc_fops = {
-	.open		= cmdline_proc_open,
-	.read		= seq_read,
-	.llseek		= seq_lseek,
-	.release	= single_release,
+		.open = cmdline_proc_open,
+		.read = seq_read,
+		.llseek = seq_lseek,
+		.release = single_release,
 };
 
 static int __init proc_cmdline_init(void)
