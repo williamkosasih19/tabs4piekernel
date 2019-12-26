1ff7242ed751569a5f9fb3d23658b13fa016752e
williamkosasih19 <williamkosasih19@gmail.com>
2019-12-26 18:19:10 +1030

    update build scripts

--- build_kernel_william.sh
+++ build_kernel_william.sh
@@ -2,7 +2,7 @@
 
 export ARCH=arm64
 export CROSS_COMPILE=/home/williamkosasih/aarch64-linux-android-4.9/bin/aarch64-linux-android-
-exoirt ANDROID_MAJOR_VERSION=p
+export ANDROID_MAJOR_VERSION=p
 export PLATFORM_VERSION=9.0.0
 
 make -C $(pwd) O=$(pwd)/out KCFLAGS=-mno-android william_defconfig
@@ -14,4 +14,4 @@ rm kernel.zip
 
 cp ../out/arch/arm64/boot/Image.gz-dtb zImage
 
-zip -r kernel.zip .
+zip -r kernel.zip *
--- pack_anyKernel.sh
+++ pack_anyKernel.sh
@@ -4,4 +4,4 @@ rm kernel.zip
 
 cp ../out/arch/arm64/boot/Image.gz-dtb zImage
 
-zip -r kernel.zip .
+zip -r kernel.zip *
